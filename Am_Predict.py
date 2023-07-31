06eeff51f06ba1afe787642415f374120d566bba
aritradey-CS <aritra1999.dey@gmail.com>
2023-07-31 23:34:52 +0530

    import pandas as pd

--- Am_Predict.py
+++ Am_Predict.py
@@ -306,68 +306,144 @@ for model_name, model in models.items():
 
 
 
-                    import pandas as pd
-                    from sklearn.model_selection import train_test_split
-                    from sklearn.ensemble import RandomForestRegressor
-                    from sklearn.linear_model import LinearRegression
-                    from sklearn.metrics import mean_absolute_error, mean_squared_error
+                    # import pandas as pd
+                    # from sklearn.model_selection import train_test_split
+                    # from sklearn.ensemble import RandomForestRegressor
+                    # from sklearn.linear_model import LinearRegression
+                    # from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-                    # Load and preprocess your dataset
-                    # df = pd.read_csv('your_dataset.csv')
-                    # ... Preprocessing steps ...
-                    # Assuming 'University Rating' is a categorical feature
-                    df['University Rank Category'] = 'U' + df['University Rating'].astype(str)
+                    # # Load and preprocess your dataset
+                    # # df = pd.read_csv('your_dataset.csv')
+                    # # ... Preprocessing steps ...
+                    # # Assuming 'University Rating' is a categorical feature
+                    # df['University Rank Category'] = 'U' + df['University Rating'].astype(str)
 
-                    # Define the categorical columns you want to one-hot encode
-                    categorical_columns = ['University Rank Category']  # Add more columns if needed
+                    # # Define the categorical columns you want to one-hot encode
+                    # categorical_columns = ['University Rank Category']  # Add more columns if needed
 
-                    # Perform one-hot encoding on categorical columns
-                    df_encoded = pd.get_dummies(df, columns=categorical_columns, drop_first=True)
+                    # # Perform one-hot encoding on categorical columns
+                    # df_encoded = pd.get_dummies(df, columns=categorical_columns, drop_first=True)
 
-                    # Define features and target variable
-                    X = df_encoded.drop('Chance of Admit ', axis=1)
-                    y = df_encoded['Chance of Admit ']
+                    # # Define features and target variable
+                    # X = df_encoded.drop('Chance of Admit ', axis=1)
+                    # y = df_encoded['Chance of Admit ']
 
-                    # Split the dataset into train and test sets
-                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+                    # # Split the dataset into train and test sets
+                    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-                    # Initialize models for analysis
-                    models = {
-                        'Random Forest': RandomForestRegressor(),
-                        'Linear Regression': LinearRegression()
-                    }
+                    # # Initialize models for analysis
+                    # models = {
+                    #     'Random Forest': RandomForestRegressor(),
+                    #     'Linear Regression': LinearRegression()
+                    # }
 
-                    # ... (previous code)
+                    # # ... (previous code)
 
-                    # Loop through each University Rank Category
-                    for category in df['University Rank Category'].unique():
-                        print(f"University Rank Category: {category}")
+                    # # Loop through each University Rank Category
+                    # for category in df['University Rank Category'].unique():
+                    #     print(f"University Rank Category: {category}")
 
-                        # Filter data for the current category
-                        column_name = 'University Rank Category_' + category
-                        # category_data = df_encoded[df_encoded[column_name] == 1]
+                    #     # Filter data for the current category
+                    #     column_name = 'University Rank Category_' + category
+                    #     # category_data = df_encoded[df_encoded[column_name] == 1]
 
-                        # Prepare datasets for the current category
-                        X_train_category = X_train[X_train.index.isin(category_data.index)]
-                        X_test_category = X_test[X_test.index.isin(category_data.index)]
-                        y_train_category = y_train[y_train.index.isin(category_data.index)]
-                        y_test_category = y_test[y_test.index.isin(category_data.index)]
+                    #     # Prepare datasets for the current category
+                    #     X_train_category = X_train[X_train.index.isin(category_data.index)]
+                    #     X_test_category = X_test[X_test.index.isin(category_data.index)]
+                    #     y_train_category = y_train[y_train.index.isin(category_data.index)]
+                    #     y_test_category = y_test[y_test.index.isin(category_data.index)]
 
-                        # Train and evaluate selected models
-                        for model_name, model in models.items():
-                            print(f"\nModel: {model_name}")
+                    #     # Train and evaluate selected models
+                    #     for model_name, model in models.items():
+                    #         print(f"\nModel: {model_name}")
 
-                            # Train the model
-                            model.fit(X_train_category, y_train_category)
+                    #         # Train the model
+                    #         model.fit(X_train_category, y_train_category)
 
-                            # Make predictions
-                            y_pred = model.predict(X_test_category)
+                    #         # Make predictions
+                    #         y_pred = model.predict(X_test_category)
 
-                            # Evaluate performance using MAE, MSE, and RMSE
-                            mae = mean_absolute_error(y_test_category, y_pred)
-                            mse = mean_squared_error(y_test_category, y_pred)
-                            rmse = mean_squared_error(y_test_category, y_pred, squared=False)
+                    #         # Evaluate performance using MAE, MSE, and RMSE
+                    #         mae = mean_absolute_error(y_test_category, y_pred)
+                    #         mse = mean_squared_error(y_test_category, y_pred)
+                    #         rmse = mean_squared_error(y_test_category, y_pred, squared=False)
 
-                            print("Mean Absolute Error:", mae)
-                            print("Mean Squared Error:", mse)
-                            print("Root Mean Squared Error:", rmse)
+                    #         print("Mean Absolute Error:", mae)
+                    #         print("Mean Squared Error:", mse)
+                    #         print("Root Mean Squared Error:", rmse)
+
+
+#  Now I created a bar chart showing the Mean Squared Error (MSE) for each model and each University Rank
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Load and preprocess your dataset
+# df = pd.read_csv('your_dataset.csv')
+# ... Preprocessing steps ...
+# Assuming 'University Rating' is a categorical feature
+df['University Rank Category'] = 'U' + df['University Rating'].astype(str)
+
+# Define the categorical columns you want to one-hot encode
+categorical_columns = ['University Rank Category']  # Add more columns if needed
+
+# Perform one-hot encoding on categorical columns
+df_encoded = pd.get_dummies(df, columns=categorical_columns, drop_first=True)
+
+# Define features and target variable
+X = df_encoded.drop('Chance of Admit ', axis=1)
+y = df_encoded['Chance of Admit ']
+
+# Split the dataset into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize models for analysis
+models = {
+    'Random Forest': RandomForestRegressor(),
+    'Linear Regression': LinearRegression()
+}
+
+# Create a dictionary to store MSE values for each model and category
+mse_results = {model_name: [] for model_name in models.keys()}
+
+# Loop through each University Rank Category
+for category in df['University Rank Category'].unique():
+    print(f"University Rank Category: {category}")
+
+    # Filter data for the current category
+    column_name = 'University Rank Category_' + category
+    # category_data = df_encoded[df_encoded[column_name] == 1]
+
+    # Prepare datasets for the current category
+    X_train_category = X_train[X_train.index.isin(category_data.index)]
+    X_test_category = X_test[X_test.index.isin(category_data.index)]
+    y_train_category = y_train[y_train.index.isin(category_data.index)]
+    y_test_category = y_test[y_test.index.isin(category_data.index)]
+
+    # Train and evaluate selected models
+    for model_name, model in models.items():
+        print(f"\nModel: {model_name}")
+
+        # Train the model
+        model.fit(X_train_category, y_train_category)
+
+        # Make predictions
+        y_pred = model.predict(X_test_category)
+
+        # Calculate MSE
+        mse = mean_squared_error(y_test_category, y_pred)
+        mse_results[model_name].append(mse)
+
+# Create a DataFrame from the mse_results dictionary
+mse_df = pd.DataFrame(mse_results)
+
+# Plot a bar chart
+mse_df.plot(kind='bar', xlabel='University Rank Category', ylabel='Mean Squared Error',
+            title='Mean Squared Error by Model and University Rank Category')
+plt.xticks(rotation=0)
+plt.legend(title='Model')
+plt.show()
+ 
